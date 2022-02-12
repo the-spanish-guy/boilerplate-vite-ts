449ae5dcc148d5a818c3b9c637090861e65b7ab2
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2022-02-12 20:27:54 -0300

    :sparkles: feat: update paths imports
    - update path imports
    - create home page

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,15 +1,22 @@
 import { FC, ReactElement } from 'react'
 import { Helmet } from 'react-helmet'
 
+import catIcon from '@/assets/cat.png'
+import dogIcon from '@/assets/dog.png'
+import useStore from '@/stores/useStore'
+
 type LayoutType = {
   title: string
 }
 
 const LayoutComponent: FC<LayoutType> = ({ title, children }): ReactElement => {
+  const fav = useStore(state => state.favicon)
+
   return (
     <>
       <Helmet>
         <meta charSet="utf-8" />
+        <link rel="icon" href={fav === 'cat' ? catIcon : dogIcon} />
         <title>{title}</title>
       </Helmet>
       {children}
--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,20 +1,71 @@
-import Counter from '../../components/Counter/Index'
-import Header from '../../components/Header/Index'
-import LayoutComponent from '../../components/Layout'
-import useStore from '../../stores/useStore'
+import { useEffect, useState } from 'react'
+import Counter from '@/components/Counter/Index'
+import LayoutComponent from '@/components/Layout'
+import CatService from '@/services/CatService'
+import DogService from '@/services/DogService'
+import useStore from '@/stores/useStore'
 import './index.css'
 
 export function Home() {
+  const [image, setImage] = useState('')
+
   const counter = useStore(state => state.setCounter)
+  const setFavicon = useStore(state => state.setFavicon)
   const handleClick = () => {
     counter(1)
   }
+
+  const handleCatApi = async () => {
+    return await new CatService().getCat().then(data => {
+      setImage(data[0].url)
+      setFavicon('cat')
+    })
+  }
+
+  const handleDogApi = async () => {
+    return await new DogService().getDog().then(({ message }) => {
+      setImage(message)
+      setFavicon('dog')
+    })
+  }
+
+  useEffect(() => {
+    handleDogApi()
+  }, [])
+
   return (
     <LayoutComponent title="Home">
-      <Header />
-      <Counter />
-      <h1 className="teste">Hello world!</h1>
-      <button onClick={handleClick}>Contador</button>
+      <div className="flex flex-col items-center justify-center w-screen h-screen">
+        <h1 className="title">Vite + React + TailwindCSS + Zustand!</h1>
+        <div className="flex justify-center w-full">
+          <img className="w-[350px] h-full rounded" src={image} alt="" />
+        </div>
+        <div className="flex flex-col items-center">
+          <div>
+            <button
+              className="w-20 m-2 p-3 text-[#5E69EE] font-bold bg-[white] rounded"
+              onClick={handleCatApi}
+            >
+              Cat
+            </button>
+            <button
+              className="w-20 m-2 p-3 text-[#5E69EE] font-bold bg-[white] rounded"
+              onClick={handleDogApi}
+            >
+              Dog
+            </button>
+          </div>
+          <div>
+            <button
+              className="m-2 p-3 text-white font-bold bg-[#39AFEA] rounded w-44"
+              onClick={handleClick}
+            >
+              Contador
+            </button>
+            <Counter />
+          </div>
+        </div>
+      </div>
     </LayoutComponent>
   )
 }
