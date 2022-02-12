0cc210553a7e00f1478c2e4942206cd6c4c61937
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2022-02-12 19:09:07 -0300

    :construction_worker: build: improve vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  preview: {
-    port: 8080,
-    https: false,
-    cors: false
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [react()],
+    preview: {
+      port: 8080,
+      https: false,
+      cors: false
+    },
+    define: {
+      'process.env': Object.entries(env).reduce((prev, [key, val]) => {
+        return {
+          ...prev,
+          [key]: val
+        }
+      }, {})
+    }
   }
 })
