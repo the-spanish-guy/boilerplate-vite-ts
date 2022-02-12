c678ee92509035bffb8ff009cf7187e5b8e3fb0e
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2022-02-12 20:26:38 -0300

    :sparkles: feat: add store

--- src/stores/useStore.ts
+++ src/stores/useStore.ts
@@ -3,12 +3,18 @@ import create from 'zustand'
 type State = {
   counter: number
   setCounter: (counter: number) => void
+  favicon: string
+  setFavicon: (favicon: string) => void
 }
 
 const useStore = create<State>(set => ({
   counter: 0,
   setCounter: counter => {
-    set(state => ({ counter: state.counter + 1 }))
+    set(state => ({ counter: state.counter + counter }))
+  },
+  favicon: 'cat',
+  setFavicon: favicon => {
+    set(state => ({ favicon: favicon }))
   }
 }))
 
