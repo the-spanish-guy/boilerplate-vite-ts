1c54da201d2de63e1f8a0cb203dc2992b5987feb
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2022-02-12 19:10:53 -0300

    :sparkles: feat: add services

---
+++ src/services/CatService.ts
@@ -0,0 +1,14 @@
+import { ICatApi } from '../interfaces/CatApiInterface'
+import BasicService from './HttpServices'
+
+class CatService extends BasicService {
+  constructor() {
+    super(process.env.VITE_CAT_DOMAIN as string)
+  }
+
+  public async getCat(): Promise<ICatApi[]> {
+    return (await this.connection.get<ICatApi[]>('/search')).data
+  }
+}
+
+export default CatService
