919c7bf6a771c7f98a7097ec71465bb85df48b85
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2022-02-12 20:25:45 -0300

    :sparkles: feat: add paths config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -18,6 +19,11 @@ export default defineConfig(({ mode }) => {
           [key]: val
         }
       }, {})
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
     }
   }
 })
